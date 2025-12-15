49c60a76a2f9fad2fe20e654d7fdae279ab7d800
Felix Weuthen <felixweuth@gmail.com>
2025-12-15 14:42:59 +0100

    feat: Added speech_recorder basic package

---
+++ packages/speech_recorder/example/android/app/src/main/kotlin/com/example/speech_recorder_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.speech_recorder_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ packages/speech_recorder/example/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:hippo_components/hippo_components.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(App(brightness: Brightness.light, home: _HomePage()));
+}
+
+class _HomePage extends StatelessWidget {
+  const _HomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageContainer(title: 'Speech Recorder Example', body: Placeholder());
+  }
+}

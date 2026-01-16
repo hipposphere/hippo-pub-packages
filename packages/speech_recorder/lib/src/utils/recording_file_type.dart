6921646faef86839460023811eb8a61c4e116bf0
Felix Weuthen <felixweuth@gmail.com>
2026-01-16 13:20:47 +0100

    better speech recorder file types

--- packages/speech_recorder/lib/src/utils/recording_file_type.dart
+++ packages/speech_recorder/lib/src/utils/recording_file_type.dart
@@ -3,20 +3,22 @@ import 'package:record/record.dart';
 class RecordingFileType {
   static String fileExtensionFromAudioEncoder(AudioEncoder encoder) {
     return switch (encoder) {
-      AudioEncoder.aacLc => 'm4a',
-      AudioEncoder.flac => 'flac',
-      AudioEncoder.opus => 'opus',
-      AudioEncoder.wav => 'wav',
+      .aacLc || .aacEld || .aacHe => 'm4a',
+      .flac => 'flac',
+      .opus => 'opus',
+      .wav => 'wav',
+      .pcm16bits => 'pcm',
       _ => 'wav',
     };
   }
 
   static String mimeTypeFromAudioEncoder(AudioEncoder encoder) {
     return switch (encoder) {
-      AudioEncoder.aacLc => 'audio/m4a',
-      AudioEncoder.flac => 'audio/flac',
-      AudioEncoder.opus => 'audio/opus',
-      AudioEncoder.wav => 'audio/wav',
+      .aacLc || .aacEld || .aacHe => 'audio/aac',
+      .flac => 'audio/flac',
+      .opus => 'audio/opus',
+      .wav => 'audio/wav',
+      .pcm16bits => 'audio/wav',
       _ => 'audio/wav',
     };
   }

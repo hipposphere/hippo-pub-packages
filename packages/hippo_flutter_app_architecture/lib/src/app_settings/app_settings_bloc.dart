7db0ad8f0bb1720cca72a35419b1207e22f6b4c7
Felix Weuthen <felixweuth@gmail.com>
2026-05-15 13:06:22 +0200

    added app_settings to hippo_flutter_app_architecture

---
+++ packages/hippo_flutter_app_architecture/lib/src/app_settings/app_settings_bloc.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
+import 'package:hippo_utils/hippo_utils.dart';
+import 'app_settings.dart';
+
+const _appSettingsKey = 'app_settings';
+
+class AppSettingsBloc extends BlocBase {
+  final KeyValueStore keyValueStore;
+
+  AppSettingsBloc({required this.keyValueStore, AppSettings? initialAppSettings}) {
+    if (initialAppSettings != null) {
+      settingsSubject.add(initialAppSettings);
+    }
+    _initBloc();
+  }
+
+  final settingsSubject = DataSubject<AppSettings?>.seeded(null);
+
+  void _initBloc() async {
+    final settings = await _getStoredAppSettings();
+    settingsSubject.add(settings);
+  }
+
+  Future<void> updateAppSettings(AppSettings newSettings) async {
+    settingsSubject.add(newSettings);
+    await _storeAppSettings(newSettings);
+  }
+
+  Future<void> updateAppSettingsBuilder(
+    AppSettings Function(AppSettings currentSettings) settingsBuilder,
+  ) async {
+    final currentSettings = settingsSubject.value;
+    if (currentSettings == null) {
+      return;
+    }
+    final newSettings = settingsBuilder(currentSettings);
+    await updateAppSettings(newSettings);
+  }
+
+  Future<AppSettings> _getStoredAppSettings() async {
+    final data = await keyValueStore.getString(_appSettingsKey);
+    if (data == null) {
+      return AppSettings.$default;
+    }
+    return AppSettings.fromData(jsonDecode(data));
+  }
+
+  Future<void> _storeAppSettings(AppSettings appSettings) async {
+    await keyValueStore.setString(_appSettingsKey, jsonEncode(appSettings.toData()));
+  }
+
+  @override
+  void dispose() {
+    settingsSubject.close();
+  }
+
+  static AppSettingsBloc of(BuildContext context) => BlocProvider.of<AppSettingsBloc>(context);
+}

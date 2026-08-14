2c42c4d920aae34cbfb0043874477399d4f40ea3
flowhorn <felixweuth@gmail.com>
2026-08-14 08:34:07 +0200

    remove hotkey api

--- packages/hotkey_api/lib/src/api/hotkey_api.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hotkey_api/hotkey_api.dart';
-import 'hotkey_api_platform_interface.dart';
-
-class HotkeyApi {
-  static Stream<HotkeyEvent> streamHotkeyEvents() {
-    return HotkeyApiPlatform.instance.streamHotkeyEvents();
-  }
-}
